6468191385d08135a7f04d511ae2e16637dc8d4d
Simon Truscott <saitora@gmail.com>
2025-02-22 20:01:02 +1100

    Use Clang's -fuse-ld option for LLD

--- cmake-init.sh
+++ cmake-init.sh
@@ -11,9 +11,9 @@ cmake \
   -D CMAKE_C_COMPILER=${usr_local_opt_bin}/clang \
   -D CMAKE_CXX_COMPILER=${usr_local_opt_bin}/clang++ \
   -D CMAKE_CXX_FLAGS=-isystem\ ${usr_local_include} \
-  -D CMAKE_EXE_LINKER_FLAGS=-L${usr_local_lib} \
-  -D CMAKE_MODULE_LINKER_FLAGS=-L${usr_local_lib} \
-  -D CMAKE_SHARED_LINKER_FLAGS=-L${usr_local_lib} \
+  -D CMAKE_EXE_LINKER_FLAGS=-L${usr_local_lib}\ -fuse-ld=lld \
+  -D CMAKE_MODULE_LINKER_FLAGS=-L${usr_local_lib}\ -fuse-ld=lld \
+  -D CMAKE_SHARED_LINKER_FLAGS=-L${usr_local_lib}\ -fuse-ld=lld \
   -D CMAKE_INSTALL_PREFIX=${usr_local} \
   -D CMAKE_LINKER=${usr_local_bin}/ld.lld \
   ..
