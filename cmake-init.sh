54b15d92afa853e69e2d7a1644d6d564e97f2f24
Simon Truscott <saitora@gmail.com>
2025-02-22 19:42:56 +1100

    Add include, lib and bin path variables

--- cmake-init.sh
+++ cmake-init.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 usr_local=/usr/local
+usr_local_include=${usr_local}/include
+usr_local_lib=${usr_local}/lib
 usr_local_opt=${usr_local}/opt/llvm
+usr_local_opt_bin=${usr_local_opt}/bin
 
 cmake \
-  -D CMAKE_C_COMPILER=${usr_local_opt}/bin/clang \
-  -D CMAKE_CXX_COMPILER=${usr_local_opt}/bin/clang++ \
-  -D CMAKE_CXX_FLAGS=-isystem\ ${usr_local}/include \
-  -D CMAKE_EXE_LINKER_FLAGS=-L${usr_local}/lib \
-  -D CMAKE_MODULE_LINKER_FLAGS=-L${usr_local}/lib \
-  -D CMAKE_SHARED_LINKER_FLAGS=-L${usr_local}/lib \
+  -D CMAKE_C_COMPILER=${usr_local_opt_bin}/clang \
+  -D CMAKE_CXX_COMPILER=${usr_local_opt_bin}/clang++ \
+  -D CMAKE_CXX_FLAGS=-isystem\ ${usr_local_include} \
+  -D CMAKE_EXE_LINKER_FLAGS=-L${usr_local_lib} \
+  -D CMAKE_MODULE_LINKER_FLAGS=-L${usr_local_lib} \
+  -D CMAKE_SHARED_LINKER_FLAGS=-L${usr_local_lib} \
   -D CMAKE_INSTALL_PREFIX=${usr_local} \
-  -D CMAKE_LINKER=${usr_local_opt}/bin/ld.lld \
+  -D CMAKE_LINKER=${usr_local_opt_bin}/ld.lld \
   ..
