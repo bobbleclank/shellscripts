b13f0e036bf96a2e3c551bb2f21929b5b4f5404d
Simon Truscott <saitora@gmail.com>
2025-05-04 04:45:46 +1000

    Add shell script for Clang-Format

---
+++ clang-format.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for dir in "."
+do
+  echo $dir
+  find $dir -maxdepth 1 -iname '*.h' -o -iname '*.cpp' | xargs clang-format -i -style=file
+done
+
+for dir in "include" "lib" "src" "test" "example"
+do
+  echo $dir
+  find $dir -iname '*.h' -o -iname '*.cpp' | xargs clang-format -i -style=file
+done
