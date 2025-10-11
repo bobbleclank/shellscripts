49bb7a9590ac41799b1906bda0bf04f0d7ff1096
Simon Truscott <saitora@gmail.com>
2025-10-12 07:49:00 +1100

    Add build type command-line argument for CMake

--- cmake-init.sh
+++ cmake-init.sh
@@ -8,6 +8,18 @@ usr_local_opt=${usr_local}/opt/llvm
 usr_local_opt_bin=${usr_local_opt}/bin
 usr_local_opt_lib=${usr_local_opt}/lib
 
+build_arg=${1:-"debug"}
+if [ "$build_arg" == "debug" ]; then
+  build_type=Debug
+  build_path=build/debug
+elif [ "$build_arg" == "release" ]; then
+  build_type=Release
+  build_path=build/release
+else
+  echo "Error: Invalid build type"
+  exit 1
+fi
+
 cmake \
   -D CMAKE_C_COMPILER=${usr_local_opt_bin}/clang \
   -D CMAKE_CXX_COMPILER=${usr_local_opt_bin}/clang++ \
@@ -17,6 +29,6 @@ cmake \
   -D CMAKE_SHARED_LINKER_FLAGS=-L${usr_local_lib}\ -L${usr_local_opt_lib}/c++\ -L${usr_local_opt_lib}/unwind\ -lunwind\ -fuse-ld=lld \
   -D CMAKE_INSTALL_PREFIX=${usr_local} \
   -D CMAKE_LINKER=${usr_local_bin}/ld.lld \
-  -D CMAKE_BUILD_TYPE=Debug \
-  -B build/debug \
+  -D CMAKE_BUILD_TYPE=${build_type} \
+  -B ${build_path} \
   -S .
