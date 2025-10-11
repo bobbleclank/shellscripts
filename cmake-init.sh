fcb03f30ee2a205b65d3e01ac98183a76723e12a
Simon Truscott <saitora@gmail.com>
2025-10-12 07:35:36 +1100

    Set build type to Debug for CMake

--- cmake-init.sh
+++ cmake-init.sh
@@ -17,5 +17,6 @@ cmake \
   -D CMAKE_SHARED_LINKER_FLAGS=-L${usr_local_lib}\ -L${usr_local_opt_lib}/c++\ -L${usr_local_opt_lib}/unwind\ -lunwind\ -fuse-ld=lld \
   -D CMAKE_INSTALL_PREFIX=${usr_local} \
   -D CMAKE_LINKER=${usr_local_bin}/ld.lld \
+  -D CMAKE_BUILD_TYPE=Debug \
   -B build \
   -S .
