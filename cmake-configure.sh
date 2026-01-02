773978565d3ddad43c97773cf02ce444da25a8d7
Simon Truscott <saitora@gmail.com>
2026-01-03 08:27:43 +1100

    Rename path variables in CMake shell script

--- cmake-configure.sh
+++ cmake-configure.sh
@@ -4,19 +4,19 @@ usr_local=/usr/local
 usr_local_bin=${usr_local}/bin
 usr_local_include=${usr_local}/include
 usr_local_lib=${usr_local}/lib
-usr_local_opt=${usr_local}/opt/llvm
-usr_local_opt_bin=${usr_local_opt}/bin
-usr_local_opt_lib=${usr_local_opt}/lib
+usr_local_opt_llvm=${usr_local}/opt/llvm
+usr_local_opt_llvm_bin=${usr_local_opt_llvm}/bin
+usr_local_opt_llvm_lib=${usr_local_opt_llvm}/lib
 
 preset_name=${1:-"debug"}
 
 cmake \
-  -D CMAKE_C_COMPILER=${usr_local_opt_bin}/clang \
-  -D CMAKE_CXX_COMPILER=${usr_local_opt_bin}/clang++ \
+  -D CMAKE_C_COMPILER=${usr_local_opt_llvm_bin}/clang \
+  -D CMAKE_CXX_COMPILER=${usr_local_opt_llvm_bin}/clang++ \
   -D CMAKE_CXX_FLAGS=-isystem\ ${usr_local_include} \
-  -D CMAKE_EXE_LINKER_FLAGS=-L${usr_local_lib}\ -L${usr_local_opt_lib}/c++\ -L${usr_local_opt_lib}/unwind\ -lunwind\ -fuse-ld=lld \
-  -D CMAKE_MODULE_LINKER_FLAGS=-L${usr_local_lib}\ -L${usr_local_opt_lib}/c++\ -L${usr_local_opt_lib}/unwind\ -lunwind\ -fuse-ld=lld \
-  -D CMAKE_SHARED_LINKER_FLAGS=-L${usr_local_lib}\ -L${usr_local_opt_lib}/c++\ -L${usr_local_opt_lib}/unwind\ -lunwind\ -fuse-ld=lld \
+  -D CMAKE_EXE_LINKER_FLAGS=-L${usr_local_lib}\ -L${usr_local_opt_llvm_lib}/c++\ -L${usr_local_opt_llvm_lib}/unwind\ -lunwind\ -fuse-ld=lld \
+  -D CMAKE_MODULE_LINKER_FLAGS=-L${usr_local_lib}\ -L${usr_local_opt_llvm_lib}/c++\ -L${usr_local_opt_llvm_lib}/unwind\ -lunwind\ -fuse-ld=lld \
+  -D CMAKE_SHARED_LINKER_FLAGS=-L${usr_local_lib}\ -L${usr_local_opt_llvm_lib}/c++\ -L${usr_local_opt_llvm_lib}/unwind\ -lunwind\ -fuse-ld=lld \
   -D CMAKE_LINKER=${usr_local_bin}/ld.lld \
   -D CMAKE_INSTALL_PREFIX=${usr_local} \
   -S . \
