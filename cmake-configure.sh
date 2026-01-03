fac10aabfef74c34ff7a57f0a43768fa868c6175
Simon Truscott <saitora@gmail.com>
2026-01-03 19:07:39 +1100

    Change path for LLVM's linker in CMake shell script

--- cmake-configure.sh
+++ cmake-configure.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 usr_local=/usr/local
-usr_local_bin=${usr_local}/bin
 usr_local_include=${usr_local}/include
 usr_local_lib=${usr_local}/lib
 usr_local_opt=${usr_local}/opt
 usr_local_opt_llvm=${usr_local_opt}/llvm
 usr_local_opt_llvm_bin=${usr_local_opt_llvm}/bin
 usr_local_opt_llvm_lib=${usr_local_opt_llvm}/lib
+usr_local_opt_lld=${usr_local_opt}/lld
+usr_local_opt_lld_bin=${usr_local_opt_lld}/bin
 
 preset_name=${1:-"debug"}
 
@@ -18,7 +19,7 @@ cmake \
   -D CMAKE_EXE_LINKER_FLAGS=-L${usr_local_lib}\ -L${usr_local_opt_llvm_lib}/c++\ -L${usr_local_opt_llvm_lib}/unwind\ -lunwind\ -fuse-ld=lld \
   -D CMAKE_MODULE_LINKER_FLAGS=-L${usr_local_lib}\ -L${usr_local_opt_llvm_lib}/c++\ -L${usr_local_opt_llvm_lib}/unwind\ -lunwind\ -fuse-ld=lld \
   -D CMAKE_SHARED_LINKER_FLAGS=-L${usr_local_lib}\ -L${usr_local_opt_llvm_lib}/c++\ -L${usr_local_opt_llvm_lib}/unwind\ -lunwind\ -fuse-ld=lld \
-  -D CMAKE_LINKER=${usr_local_bin}/ld.lld \
+  -D CMAKE_LINKER=${usr_local_opt_lld_bin}/ld.lld \
   -D CMAKE_INSTALL_PREFIX=${usr_local} \
   -S . \
   --preset ${preset_name}
